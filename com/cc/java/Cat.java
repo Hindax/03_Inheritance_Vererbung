69442cebabe6010e5c52b6c56d0dd3b0fc873813
Hindax <haar162@more2learn.de>
2022-11-25 11:18:45 +0100

    new sub class

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,48 +1,32 @@
 package com.cc.java;
 
-public class Cat {
- 
-    private String name;
-    private String furColor;
-    private int age;
+public class Cat extends SuperCat {
 
     private int counter;
 
     public Cat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-}
+        super(name, furColor, age);
 
-    public String getStringAttributes(String flag) {
-        switch (flag) {
-            case "#name":
-                return name;
-            case "#color":
-                return furColor;
-            default:
-                return "#!ERROR";
-        }
     }
 
     public String getAge() {
-            return checkEscalationLevel(); 
-        }
+        return checkEscalationLevel();
+    }
 
     private String checkEscalationLevel() {
-    
-    counter++;
-    
+
+        counter++;
+
         switch (counter) {
             case 1:
                 return "This is an inappropriate question!";
             case 2:
-                return "I've told you once!"; 
+                return "I've told you once!";
             case 3:
-                return "Talk to the hand!";        
+                return "Talk to the hand!";
             default:
-                return "1#!?&&%"; 
+                return "1#!?&&%";
         }
     }
- 
+
 }
--- com/cc/java/TomCat.java
+++ com/cc/java/TomCat.java
@@ -1,34 +1,15 @@
 package com.cc.java;
 
-public class TomCat {
- 
-   private String name;
-   private String furColor;
-   private int age;
-
-
+public class TomCat extends SuperCat {
 
     public TomCat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
-}
+        super(name, furColor, age);
 
-    public String getStringAttributes(String flag) {
-        switch (flag) {
-            case "#name":
-                return name;
-            case "#color":
-                return furColor;
-            default:
-                return "#!ERROR";
-        }
     }
 
     public String getAge() {
-            return Integer.toString(age);
+        return Integer.toString(age);
 
-        }
-   
- 
+    }
+
 }
